4d3d4bc45d271eb2fbaff365ce62d21a3cca6677
OscarQQ <qyx1118@outlook.com>
2019-03-23 01:59:10 -0700

    having fun

--- webpage/main.js
+++ webpage/main.js
@@ -1,8 +1,31 @@
-document.getElementById('loginBtn').onmousedown = print;
+var dict = {"efe": 12345, "oscar": "andi"};
+document.getElementById('loginBtn').onmousedown = checkuser;
 
-function print(){
-    var x = document.getElementById('username').value;
-    console.log(x);
-    var y = document.getElementById('password').value;
-    console.log(y);
+function checkuser(){
+    var user = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    console.log(user);
+    console.log(password);
+
+    if(user == ''){
+        document.getElementById('error').innerHTML="Please Enter Username";
+        document.getElementById('patrick').innerHTML="Opps";
+        return false;
+    }
+    if(password == ''){
+        document.getElementById('error').innerHTML="Please Enter Password";
+        document.getElementById('patrick').innerHTML="Woww";
+        return false;
+    }
+    if(dict[user] == password){
+        document.getElementById('error').innerHTML="";
+        document.getElementById('patrick').innerHTML="Go to bed!!!";
+        console.log("success");
+        return true;
+    }else{
+        document.getElementById('error').innerHTML="Mismatch, Enter Again";
+        document.getElementById('patrick').innerHTML="I love U";
+        return false;
+    }  
 }
