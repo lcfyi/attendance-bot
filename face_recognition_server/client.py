8a3dc45ef96f19993bf3e83b805911d7e0713255
jaronsander <jar.sand.on@gmail.com>
2019-03-29 01:09:24 -0700

    added client code

--- face_recognition_server/client.py
+++ face_recognition_server/client.py
@@ -5,10 +5,13 @@ import multiprocessing
 import threading
 import numpy as np
 import pickle 
+import RobotMotion.track_bot as bot
+import json
 
 THREADS = []
 
 image = None
+dick = {"angle":2, "move":1.5, "max":120, "min":60}
 
 def camera(signal):
     global image
@@ -21,42 +24,75 @@ def camera(signal):
         r, img = vc.read()
         image = cv2.flip(img, -1)
 
-def websocket(signal):
-    async def oof():
-        ws = await websockets.connect("ws://cpen291-16.ece.ubc.ca/ws/rpicam")
-        while signal.is_set():
-            try:
-                if not signal.is_set():
-                    return
-                if not ws.open and signal.is_set():
-                    print("Not open")
-                    ws = await websockets.connect("ws://cpen291-16.ece.ubc.ca/ws/rpicam")
-                if image is not None:
-                    try:
-                        await asyncio.sleep(0.2)
-                        img = image.tobytes()
-                        await asyncio.wait_for(ws.send(img), 0.2)
-                    except asyncio.TimeoutError:
-                        print("Timeout")
-                        ws.close()
-            except websockets.exceptions.ConnectionClosed:
-                pass
+def robot():
+    rob = bot.TrackRobot()
+    rob.servoTime(dick["angle"])
+    rob.moveTime(dick["move"])
+    rob.changeAngles(dick["max"], dick["min"])
 
-    asyncio.new_event_loop().run_until_complete(oof())
-    asyncio.get_event_loop().run_forever()
+async def paramAsy(signal):
+    ws = await websockets.connect("/ws/signal/rpi")
+    while signal.is_set():
+        try:
+            if not signal.is_set():
+                return
+            if not ws.open and signal.is_set():
+                print("not open")
+                ws = await websockets.connect("/ws/signal/rpi")
+            try:
+                while True:
+                    dictionary = await websockets.recv()
+                    val = json.loads(dictionary)
+                    dick["angle"] = val["angle"]
+                    dick["move"] = val["move"]
+                    dick["max"] = val["max"]
+                    dick["min"] = val["min"]
+            except asyncio.TimeoutError:
+                print("Timeout")
+                ws.close()
+        except websockets.exceptions.ConnectionClosed:
+            print("Socket Closed")
+
+async def camAsy(signal):
+    ws = await websockets.connect("ws://cpen291-16.ece.ubc.ca/ws/rpicam")
+    while signal.is_set():
+        try:
+            if not signal.is_set():
+                return
+            if not ws.open and signal.is_set():
+                print("Not open")
+                ws = await websockets.connect("ws://cpen291-16.ece.ubc.ca/ws/rpicam")
+            if image is not None:
+                try:
+                    await asyncio.sleep(0.2)
+                    img = image.tobytes()
+                    await asyncio.wait_for(ws.send(img), 0.2)
+                except asyncio.TimeoutError:
+                    print("Timeout")
+                    ws.close()
+        except websockets.exceptions.ConnectionClosed:
+            pass
 
 def main(signal):
+    manager = multiprocessing.Manager()
+    dic = manager.dict()
+
+    bot_process = multiprocessing.Process(target=robot, args=(dic,))
+
+    PROCESSES.append(bot_process)
     cam = threading.Thread(target=camera, args=(signal,))
-    socks = threading.Thread(target=websocket, args=(signal,))
 
     THREADS.append(cam)
-    THREADS.append(socks)
     for t in THREADS:
         t.start()
-    
+    for p in PROCESSES:
+        p.start()
+
+    await asyncio.gather(paramAsy(signal), camAsy(signal))
+
     while True:
         pass
-
+asyncio.run(main(signal()))
 
 if __name__ == "__main__":
     try:
@@ -67,3 +103,5 @@ if __name__ == "__main__":
         signal.clear()
         for t in THREADS:
             t.join()
+        for p in PROCESSES:
+            p.terminate()
