ec9a3d1a92c07e7774620eb92cd8595d825835c0
lcheng1 <3242609+lcheng1@users.noreply.github.com>
2019-03-29 05:55:47 -0700

    Updated unclaimed logic for better filtering.

--- webpage/teacher/request_db_entries.php
+++ webpage/teacher/request_db_entries.php
@@ -17,7 +17,7 @@
             }
         } else if ($_POST['requestTable'] === "unclaimed") {
             //request entries in the format Login Phrase to show all unused login_phrases
-            $sql_query = "SELECT `Login_Phrase` FROM `student_info` WHERE Needs_Update = '1' AND `studentID`=NULL";
+            $sql_query = "SELECT `Login_Phrase` FROM `student_info` WHERE Needs_Update = '1' AND Name IS NULL";
 
             // If fetch was successful, return the table
             if ($result = mysqli_query($db, $sql_query)) {
